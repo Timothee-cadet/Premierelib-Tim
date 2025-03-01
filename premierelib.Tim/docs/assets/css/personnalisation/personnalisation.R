48e0bc4d3cead1e490a266ac4e4b2195eaa9788d
Timothée <timothee.cadet8@gmail.com>
2025-03-01 23:50:33 +0100

    Ajout des fichiers générés pour le site

---
+++ premierelib.Tim/docs/assets/css/personnalisation/personnalisation.R
@@ -0,0 +1,21 @@
+/* Style de la barre de navigation (les onglets) */
+  .navbar {
+    background-color: #007bff !important; /* Bleu vif pour la barre de navigation */
+  }
+
+/* Couleur du texte des onglets */
+  .navbar a {
+    color: white !important; /* Texte des onglets en blanc */
+      font-weight: bold; /* Optionnel: mettre les onglets en gras */
+  }
+
+/* Couleur au survol des onglets */
+  .navbar a:hover {
+    background-color: #0056b3; /* Bleu plus foncé au survol */
+      color: white !important; /* Garder le texte en blanc même au survol */
+  }
+
+/* Onglet actif */
+  .navbar .active {
+    background-color: #004085; /* Un bleu encore plus foncé pour l'onglet actif */
+  }
